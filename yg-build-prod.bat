d7e723ae1e8037d595267f1b5cc415a4464376e1
litaojian <litaojian88@gmail.com>
2018-01-03 14:57:55 +0800

    ok

---
+++ yg-build-prod.bat
@@ -0,0 +1,4 @@
+call ng build --prod --no-extract-license --environment prod --base-href ./
+copy /Y dist E:\MySVN\SF-DEMO\ZxWorkReport\html\static
+pause
+ 
---
+++ yg-build.bat
@@ -0,0 +1,4 @@
+call ng build --environment prod --base-href ./
+copy /Y dist E:\MySVN\SF-DEMO\ZxWorkReport\html\static
+pause
+ 
