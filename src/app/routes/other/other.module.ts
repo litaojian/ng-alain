d2885586aa54a2eac00f53037495ce22c9711e2f
litaojian <litaojian88@gmail.com>
2018-01-03 15:47:35 +0800

    ok

--- src/app/routes/other/other.module.ts
+++ src/app/routes/other/other.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared/shared.module';
 import { AngularSplitModule } from 'angular-split';
-import { NzClipboardModule } from 'ng-clipboard-antd';
 
 import { OtherRoutingModule } from './other-routing.module';
 import { SplitComponent } from './split/split.component';
-import { ClipboardComponent } from './clipboard/clipboard.component';
 
 @NgModule({
   imports: [
@@ -14,8 +12,7 @@ import { ClipboardComponent } from './clipboard/clipboard.component';
     SharedModule,
     OtherRoutingModule,
     AngularSplitModule,
-    NzClipboardModule.forRoot()
   ],
-  declarations: [SplitComponent, ClipboardComponent]
+  declarations: [SplitComponent]
 })
 export class OtherModule { }
