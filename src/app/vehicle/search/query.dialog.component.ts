4c6f94194092658e86ec67cf3df0d832006dd033
litaojian <litaojian88@gmail.com>
2018-01-10 08:41:14 +0800

    ok

--- src/app/vehicle/search/query.dialog.component.ts
+++ src/app/vehicle/search/query.dialog.component.ts
@@ -2,8 +2,8 @@ import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import { _HttpClient } from '@delon/theme';
 
 @Component({
-    selector: 'vehicle-search',
-    templateUrl: './search.component.html',
+    selector: 'vehicle-search-query',
+    templateUrl: './query.dialog.html',
     styleUrls: ['./search.component.less']
 })
 export class QueryDialogComponent implements OnInit {
--- src/app/vehicle/vehicleSearch.module.ts
+++ src/app/vehicle/vehicleSearch.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
 import { SearchComponent } from './search/search.component';
-
+import { QueryDialogComponent } from './search/query.dialog.component';
 
 const routes: Routes = [
     { path: 'search', component: SearchComponent,data: { title: '过车查询' } }
@@ -15,7 +15,11 @@ const routes: Routes = [
     ],
     providers: [],
     declarations: [
-        SearchComponent
+        SearchComponent,
+        QueryDialogComponent
+    ],
+    entryComponents:[
+        QueryDialogComponent
     ],
     exports: [
         RouterModule
