2cb75ee99118101a3057969af17ae0b05eda18be
liangjunj <953280311@qq.com>
2018-01-11 15:41:40 +0800

    修改车辆布控编辑页面

--- src/app/vehicle/VehicleControlRequest/vehicleControl.module.ts
+++ src/app/vehicle/VehicleControlRequest/vehicleControl.module.ts
@@ -12,7 +12,7 @@ import { ControlListService } from "./controlList.service";
 const routes = [
     { path:'list', component:ControlListComponent, data:{ title: '布控列表' }},
     { path:'create', component:ControlModifyComponent,data:{ title:'布控新增'}},
-    { path:'edit/:id', component:ControlModifyComponent,data:{ title:'布控编辑'}}
+    { path:'edit', component:ControlModifyComponent,data:{ title:'布控编辑'}}
 ];
 
 @NgModule({
