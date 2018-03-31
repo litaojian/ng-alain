89e5d2d585917cf5d70194d944d4947bf1d656c7
Li Taojian <litaojian@139.com>
2018-03-31 14:23:55 +0800

    ok

--- src/app/admin/yg-bizpage.module.ts
+++ src/app/admin/yg-bizpage.module.ts
@@ -4,19 +4,20 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
-import { BizQueryComponent, BizQueryService,BizFormComponent, BizFormService } from 'ngx-widget/biz-app';
+import { BizQueryComponent, BizQueryService,BizFormComponent, BizFormService, BizTreeTableComponent, BizPageGuardService } from 'ngx-widget/biz-app';
 
 const routes: Routes = [{
   path: '',
-  children: [
-     { path: 'test/dialog/:id', component: BizQueryComponent },        
-     { path: ':dir/:pageName/list', component: BizQueryComponent },    
-     { path: ':dir/:pageName/list/:pid', component: BizQueryComponent },        
-     { path: ':dir/:pageName/create', component: BizFormComponent },     
-     { path: ':dir/:pageName/edit/:id', component: BizFormComponent },     
-     { path: ':dir/:pageName/view/:id', component: BizFormComponent }, 
+  children: [    
+    { path: ':dir/:pageName/tree', component: BizTreeTableComponent, canActivate:[BizPageGuardService] },    
+    { path: ':dir/:pageName/list', component: BizQueryComponent, canActivate:[BizPageGuardService] },    
+    { path: ':dir/:pageName/list/:pid', component: BizQueryComponent, canActivate:[BizPageGuardService] },        
+    { path: ':dir/:pageName/create', component: BizFormComponent },     
+    { path: ':dir/:pageName/edit', component: BizFormComponent },     
+    { path: ':dir/:pageName/view', component: BizFormComponent }, 
     { path: ':dir/:pageName', redirectTo: ':dir/:pageName/list', pathMatch: 'full' },  
-    { path: '', redirectTo: 'bizquery/index', pathMatch: 'full' }
+    { path: 'test/dialog/:id', component: BizQueryComponent },        
+    { path: '', redirectTo: 'admin/organization/tree', pathMatch: 'full' }
   ]
 }];
 
